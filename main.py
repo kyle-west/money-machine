ead0834ce63c83dc63a6812b552dcc498c205101
Kyle West <wes12009@byui.edu>
2018-03-15 11:27:50 -0600

    [NN] set up script to find optimal params

--- main.py
+++ main.py
@@ -9,10 +9,18 @@
 ########################################################################
 from sklearn.neural_network import MLPRegressor
 import pandas
+import output
 
-print ("LOADING DATASET")
+print("LOADING DATASET")
 data = pandas.read_csv('./data/raw_base_usd.csv', header=0)
-data = data[['EUR', 'CAD']] # filter out the unwanted currencies
+
+# opt in which curencies we care about
+data = data[[
+   "AUD","CAD","CHF","CZK","DKK",
+   "EUR","GBP","HKD","HUF","JPY",
+   "KRW","NOK","NZD","PLN","SEK",
+   "SGD","ZAR"
+]] 
 print(data.head())
 
 
@@ -22,10 +30,21 @@ print(data.head())
 print("BEGIN TRAINING")
 
 NN = MLPRegressor(
-   hidden_layer_sizes=(10,2),  activation='relu', solver='adam', alpha=0.001, batch_size='auto',
-   learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=1000, shuffle=True,
-   random_state=9, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
-   early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08
+   hidden_layer_sizes=(50,),  activation='logistic', solver='adam', 
+   alpha=0.001, batch_size='auto', learning_rate='constant', 
+   learning_rate_init=0.01, max_iter=10000, shuffle=False,
+   random_state=None, warm_start=False, momentum=0.9
 )
 
-model = NN.fit(data[:-1], data.shift(-1)[:-1])
+TEST_SIZE = 1
+TRAINING_SLICE = slice(0,-(TEST_SIZE + 1))
+TEST_SLICE = slice(-(TEST_SIZE + 1), -1)
+
+model = NN.fit(data[TRAINING_SLICE], data.shift(-1)[TRAINING_SLICE])
+
+predictions = model.predict(data[TEST_SLICE])
+
+print('ACUTAL', data[-1:], sep="\n")
+pred = pandas.DataFrame(predictions)
+pred.columns = data.columns
+print("PREDICTIONS",pred, sep = "\n")
