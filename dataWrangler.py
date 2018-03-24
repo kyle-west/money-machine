871562c90a0c14a5d1ccf5a97520f12ca37bb655
David Lobaccaro <davego44@yahoo.com>
2018-03-24 00:21:46 -0600

    Major Updates and Added NNN

--- dataWrangler.py
+++ dataWrangler.py
@@ -2,14 +2,16 @@ import pandas as pd
 import numpy as np
 
 class DataWrangler:
-	def __init__(self, filePath, windowSize, trainRatio):
+	def __init__(self, filePath, windowSize, debug=False):
 		self.windowSize = windowSize
-		self.trainRatio = trainRatio
-		self.setup(filePath)
+		self.currencyList = ["AUD","CAD","CHF","CZK","DKK","EUR","GBP","HKD","HUF","JPY","KRW","NOK","NZD","PLN","SEK","SGD","ZAR"]
+		self.setup(filePath, debug)
 
-	def setup(self, filePath):
+	def setup(self, filePath, debug):
 		self.originalData = pd.read_csv(filePath, header=0)
-		self.originalData = self.originalData[["AUD","CAD","CHF","CZK","DKK","EUR","GBP","HKD","HUF","JPY","KRW","NOK","NZD","PLN","SEK","SGD","ZAR"]]
+		self.originalData = self.originalData[self.currencyList]
+		if debug:
+			self.originalData = pd.DataFrame([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]])
 		self.reformatData()
 
 	def reformatData(self):
@@ -17,33 +19,66 @@ class DataWrangler:
 		dataColCount = len(self.originalData.columns)
 		rowCount = dataRowCount - self.windowSize
 		colCount = dataColCount * self.windowSize
-		data = np.empty(shape=[rowCount, colCount])
-		targets = np.empty(shape=[rowCount, dataColCount])
+		self.data = np.empty(shape=[rowCount, colCount])
+		self.targets = np.empty(shape=[dataColCount, rowCount])
 		for index, row in self.originalData.iterrows():
 			if (index + self.windowSize >= dataRowCount):
 				break
 			for i in range(self.windowSize):
 				row = self.originalData.iloc[[index + i]].get_values()
 				for j in range(dataColCount):
-					data[index][(i * dataColCount) + j] = row[0][j]
+					self.data[index][(i * dataColCount) + j] = row[0][j]
 			targetRow = self.originalData.iloc[[index + self.windowSize]].get_values()
 			for i in range(dataColCount):
-				targets[index][i] = targetRow[0][i]
-		self.splitData(data, targets)
+				self.targets[i][index] = targetRow[0][i]
 
-	def splitData(self, data, targets):
-		splitData = np.split(data, [int(self.trainRatio * len(data))])
-		splitTargets = np.split(targets, [int(self.trainRatio * len(targets))])
-		self.X_train = splitData[0]
-		self.X_test = splitData[1]
-		self.y_train = splitTargets[0]
-		self.y_test = splitTargets[1]
+	def addDailyData(self, dailyData):
+		self.originalData = self.originalData.append(pd.DataFrame(np.atleast_2d(dailyData)), ignore_index=True)
+
+	def getLastWindowSizedData(self):
+		return np.atleast_2d(np.ravel(self.originalData.tail(self.windowSize)))
 
 	def getOriginalData(self):
 		return self.originalData
 
-	def getSplitData(self):
-		return self.X_train, self.X_test, self.y_train, self.y_test
+	def getFormattedDataSplit(self, trainSize):
+		splitData = np.split(self.data, [int(trainSize * len(self.data))])
+		trainData = splitData[0]
+		testData = splitData[1]
+		trainTargetList = []
+		testTargetList = []
+		for i in range(len(self.targets)):
+			splitTargets = np.split(self.targets[i], [int(trainSize * len(self.targets[i]))])
+			trainTargetList.append(splitTargets[0])
+			testTargetList.append(splitTargets[1])
+		return trainData, testData, trainTargetList, testTargetList
 
 	def getWindowSize(self):
 		return self.windowSize
+
+	def getCurrencyList(self):
+		return self.currencyList
+
+if __name__ == "__main__":
+	wrangler = DataWrangler("data/raw_base_usd.csv", 2, True)
+	X_train, X_test, y_train_list, y_test_list = wrangler.getFormattedDataSplit(0.7)
+
+	print("Original Data")
+	print(wrangler.getOriginalData())
+
+	print("X_train")
+	print(X_train)
+
+	print("X_test")
+	print(X_test)
+
+	print("y_train_list")
+	for i in range(len(y_train_list)):
+		print(y_train_list[i])
+
+	print("y_test_list")
+	for i in range(len(y_test_list)):
+		print(y_test_list[i])
+
+	print("LastWindowSizedData")
+	print(wrangler.getLastWindowSizedData())
