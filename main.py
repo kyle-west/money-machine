326de8bb07e39d317def0fc484f739dd74b52f08
Kyle West <wes12009@byui.edu>
2018-03-27 11:09:29 -0600

    [MAIN] attempt at tying together all three

--- main.py
+++ main.py
@@ -12,18 +12,25 @@ from Format import Format
 from TransactionManager import TransactionManager
 from BuySellStay import BuySellStay
 
+print("SPLITTING DATA")
 wrangler = DataWrangler(windowSize=14)
 nnn = NNN(wrangler)
+
+print("TRAINING")
 nnn.train(0.7)
+
 testData = wrangler.loadTestData()
+i, num_rows = 0, len(testData)
+day0 = F.DataFrameRow_to_Dictionary(data[(i-1):i])
+lastDay = F.DataFrameRow_to_Dictionary(data[(i+num_rows+1):(i+num_rows+2)])
 manager = TransactionManager(initialInvestment=(day0, 0.40))
 bss = BuySellStay(aggressiveness = 300.0)
 
-print("TRADING")
+print("PREDICTING & TRADING")
 
 for i in range(i, (i+num_rows+1)):
-	row = data[i:(i+1)]
-	prediction = F.DataFrameRow_to_Dictionary(NN.predict(row))
+	row = testData[i:(i+1)]
+   prediction = F.twoLists_to_Dictionary(wrangler.getCurrencyList(), NN.smartPredict(row.get_values()[0]))
 	row = F.DataFrameRow_to_Dictionary(row)
 	actions = bss.getActions(row, prediction)
 	manager.makeTransactions(actions, row)
